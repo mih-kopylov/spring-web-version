9cfa1b028b7884712e277a567e7de7d1614c47c7
Mikhail Kopylov <mih.kopylov@yandex.ru>
2019-08-28 09:11:39 +0300

    Add javadoc for VersionedResource

--- src/main/java/ru/mihkopylov/spring/version/VersionedResource.java
+++ src/main/java/ru/mihkopylov/spring/version/VersionedResource.java
@@ -5,8 +5,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to version controller handlers.
+ * <br>
+ * <pre>
+ * &#64;GetMapping("/employees")
+ * &#64;VersionedResource(from=1)
+ * public List&lt;Employee&gt; getEmployeesV1(){}
+ * </pre>
+ */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface VersionedResource {
+    /**
+     * @return API version as from which the annotated handler will match a request until another handler for the same URL with a greater {@code from} parameter is defined
+     */
     int from() default 0;
 }
